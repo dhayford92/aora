d7c621c7f1a8eeb88d19ccc7d87dac83d06c8dbe
Denzel Hayford <dhayford@gmail.com>
2024-04-14 10:55:41 -1200

    latest and all post added

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -1,13 +1,81 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, FlatList, Image, RefreshControl } from 'react-native'
+import React, { useEffect } from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { useGlobalContext } from '../../context/GlobalProvider';
+import { images } from '../../constants'
+import SearchInput from '../../components/SearchInput';
+import Trending from '../../components/Trending';
+import EmptyState from '../../components/EmptyState';
+import { toast } from '../../lib/toaster';
+import { getAllPosts, getLatestPosts } from '../../lib/appwrite';
+import useAppwrite from '../../lib/useAppwrite';
+import VideoCard from '../../components/VideoCard';
 
 const Home = () => {
-  const { loading, isLogged } = useGlobalContext();
+  const { data: posts, refetch } = useAppwrite(getAllPosts);
+  const { data: latest } = useAppwrite(getLatestPosts);
+
+  const [ refreshing, setRefreshing ] = React.useState(false);
+
+  const onRefresh = async () => {
+    setRefreshing(true);
+    await refetch();
+    setRefreshing(false);
+  }
+
   return (
-    <SafeAreaView>
-      <Text>Home</Text>
+    <SafeAreaView className="bg-primary w-full h-full">
+      <FlatList
+        data={posts}
+        keyExtractor={(item) => item.$id}
+        renderItem={({ item }) => (
+          <VideoCard 
+            title={item.title}
+            thumbnail={item.thumbnail}
+            video={item.video}
+            avatar={item.users.avatar}
+            user={item.users.username}
+          />
+        )}
+
+        ListHeaderComponent={() => (
+          <View className="my-6 px-4 space-y-6">
+            <View className="justify-between items-start flex-row mb-6">
+              <View>
+                <Text className="text-sm font-pmedium text-gray-100">Welcome Back,</Text>
+                <Text className="text-2xl font-psemibold text-white">Denzel</Text>
+              </View>
+              <View className="mt-1.5">
+                <Image
+                    source={images.logoSmall}
+                    className="w-9 h-10"
+                    resizeMode='contain'
+                  />
+                </View>
+            </View>
+
+            <SearchInput/>
+
+            {/* Latest Videos */}
+            <View className="flex-1 pt-5 pb-8">
+              <Text className="text-lg font-pregular text-gray-100 mb-3">Latest Videos</Text>
+              <Trending posts={latest}/>
+            </View>
+          </View>
+        )}
+
+        ListEmptyComponent={() => (
+          <EmptyState title="No Videos Found" subtitle="No videos found for this user"/>
+        )}
+
+        refreshControl={
+          <RefreshControl
+            refreshing={refreshing}
+            onRefresh={onRefresh}
+          />
+        }
+
+      />
     </SafeAreaView>
   )
 }
